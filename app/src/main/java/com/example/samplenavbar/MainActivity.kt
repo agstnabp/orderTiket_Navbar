e7ae75b3fcc79e7d77f7a394467cc32cfc7682fb
agstnabp <agustinabayupramesti@mail.ugm.ac.id>
2023-10-30 10:45:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/samplenavbar/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.samplenavbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.samplenavbar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> replaceFragment(Home())
+                R.id.nav_recentorder -> replaceFragment(RecentOrder())
+                R.id.nav_profile -> replaceFragment(Profile())
+
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+
+}
